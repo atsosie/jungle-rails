e7a3767304f785f3101bdada493f937c6ad9eac2
Amber Tsosie <atxambra@gmail.com>
2017-05-11 23:36:17 +0000

    Updated views and controllers to include first_name and last_name.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,20 +4,20 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    user = User.new(user_params)
 
     if user.save
       session[:user_id] = user.id
       redirect_to '/'
     else
-      redirect_to '/register' #should this be '/new'?
+      redirect_to '/register'
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 
 end
