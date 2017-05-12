bdbd4f974258f2dc3183815a2ee119acb57a4194
Amber Tsosie <atxambra@gmail.com>
2017-05-12 19:57:39 +0000

    Feature added: send order email receipt.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,13 +10,13 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to '/'
     else
-      redirect_to '/login'
+      render :new
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/'
+    render :new
   end
 
 end
