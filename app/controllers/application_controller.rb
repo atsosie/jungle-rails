3f9c4e5f2e10c7c7c9bce2ceb25d7921298c9865
Amber Tsosie <atxambra@gmail.com>
2017-05-15 17:30:11 +0000

    Modified views: moved rating under image, edited link style, added user icon to top nav.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,4 +24,21 @@ class ApplicationController < ActionController::Base
     cookies[:cart]
   end
 
+  def rating_as_stars number
+    rating = number.to_i
+    case rating
+    when 1
+      "★"
+    when 2
+      "★★"
+    when 3
+      "★★★"
+    when 4
+      "★★★★"
+    when 5
+      "★★★★★"
+    end
+  end
+  helper_method :rating_as_stars
+
 end
